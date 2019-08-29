467e38592ba7601d4a96c55a8068b8a848b44985
Ivan Shirobokov <spirit-1993@list.ru>
2019-08-29 07:55:21 +0300

    Добавляет reducers

---
+++ src/reducers/MainReducer.js
@@ -0,0 +1,22 @@
+import { DATA_ERROR, DATA_SUCCESS, DATA_REQUEST } from '../actions/index'
+const initialState = {
+  isFetching: false,
+  error: null,
+  list: {},
+}
+export const MainReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case DATA_REQUEST:
+      return { ...state, isFetching: true }
+    case DATA_SUCCESS:
+      return { ...state, list: action.payload, isFetching: false }
+    case DATA_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        isFetching: false,
+      }
+    default:
+      return state
+  }
+}
