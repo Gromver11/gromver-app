165bac92bd1a20987206e806439bdf91c66701cd
Ivan Shirobokov <spirit-1993@list.ru>
2019-08-29 08:07:20 +0300

    Добавляет configureStore.js и redux-devtools в проект

---
+++ src/store/configureStore.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers/index'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import api from './middleware/api'
+
+const configureStore = preloadedState => {
+  return createStore(rootReducer, composeWithDevTools(applyMiddleware(api)))
+}
+
+export default configureStore
