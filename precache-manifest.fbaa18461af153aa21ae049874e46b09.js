a8dcdd0a5c961fd71b5d7634de3b11bb736d330c
Ivan Shirobokov <spirit-1993@list.ru>
2019-11-09 21:06:07 +0300

    Updates

---
+++ precache-manifest.fbaa18461af153aa21ae049874e46b09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4834c889c7d3a08ab4ba5d6ae6d00ddf",
+    "url": "/gromver-app/index.html"
+  },
+  {
+    "revision": "c580b7755b1b294af9da",
+    "url": "/gromver-app/static/css/main.0218e78f.chunk.css"
+  },
+  {
+    "revision": "56059eb4f61ea384ca83",
+    "url": "/gromver-app/static/js/2.70de19b0.chunk.js"
+  },
+  {
+    "revision": "c580b7755b1b294af9da",
+    "url": "/gromver-app/static/js/main.ba1b079c.chunk.js"
+  },
+  {
+    "revision": "6e36a20625a49bb4d557",
+    "url": "/gromver-app/static/js/runtime~main.16f2ea2c.js"
+  }
+]);
