d9917e18c5472574a510e80c2dfeee5ac50aa208
Ivan Shirobokov <spirit-1993@list.ru>
2020-08-22 11:13:36 +0400

    Меняет логику отображения пагинации

--- src/components/Paginate.jsx
+++ src/components/Paginate.jsx
@@ -1,39 +1,54 @@
 import React from 'react';
 import styles from './Paginate.module.css';
-import PropTypes from 'prop-types';
-const Paginate = props => {
-  if (props.currentPage === 1 && props.totalPages === null) {
+import { useSelector } from 'react-redux';
+import { selectTotalPages } from '../store/TableOfResults/selectors';
+import {selectIds} from '../store/TableOfResults/selectors'
+const Paginate = ({history, location, match}) => {
+
+
+  const currentPage = Number(match.params.info.slice(12))
+
+  const currentRep = location.search.slice(12);
+
+  const ids = useSelector(selectIds)
+
+  const handleLoadNextPageClick =  () => {
+    const nextPage = currentPage + 1;
+    history.push(`/seacrh&page=${nextPage}?repository=${currentRep}`);
+  };
+  const handleLoadPrevPageClick = () => {
+    const prevPage = currentPage - 1;
+    history.push(`/seacrh&page=${prevPage}?repository=${currentRep}`);
+  }
+  const totalPages = useSelector(selectTotalPages);
+
+  if (  ids.length!==0 && totalPages === null) {
     return (
       <div className={styles.paginate}>
         <span className={styles.count}>
-          {props.currentPage} из {props.currentPage}
+          {currentPage}  of {currentPage}
         </span>
       </div>
     );
   }
-  return (
+  return totalPages!==null ? (
+    
     <div className={styles.paginate}>
-      {props.currentPage !== 1 && (
-        <button className={`${styles.btn} btn`} onClick={props.onLoadPrevPage}>
+      {currentPage !== 1 && (
+        <button className={`${styles.btn} btn`} onClick={handleLoadPrevPageClick}>
           {'<<'}
         </button>
       )}
       <span className={styles.count}>
-        {props.currentPage} of {props.totalPages}
+        {currentPage} of {totalPages}
       </span>
-      {props.currentPage !== props.totalPages && (
-        <button className={`${styles.btn} btn`} onClick={props.onLoadNextPage}>
+      {currentPage !== totalPages && (
+        <button className={`${styles.btn} btn`} onClick={handleLoadNextPageClick}>
           {'>>'}
         </button>
       )}
     </div>
-  );
-};
-Paginate.propTypes = {
-  currentPage: PropTypes.number.isRequired,
-  onLoadNextPage: PropTypes.func.isRequired,
-  onLoadPrevPage: PropTypes.func.isRequired,
-  totalPages: PropTypes.number,
+  ) : null;
 };
 
-export default Paginate;
+export default Paginate
--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FormContainer from './containers/FormContainer';
-import TableOfResultContainer from './containers/TableOfResultContainer';
+import UserForm from './components/UserForm';
+import TableOfResult from './components/TableOfResults';
 import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
 import { Route, BrowserRouter } from 'react-router-dom';
 import './index.css';
+import Paginate from './components/Paginate';
 
 const store = configureStore();
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <div className="main-content">
-        <Route path="/" component={FormContainer} />
-        <Route path="/:info" component={TableOfResultContainer} />
+        <Route path="/" component={UserForm} />
+        <Route path="/:info" component={TableOfResult} />
+        <Route path="/:info" component={Paginate} />
       </div>
     </BrowserRouter>
   </Provider>,
